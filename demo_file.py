877d0f258be2acc6e79553cc7f022afed3dbd411
vikaschavan724 <145338850+vikaschavan724@users.noreply.github.com>
2024-03-13 16:52:49 +0530

    Create demo_file.py
    first demo file

---
+++ demo_file.py
@@ -0,0 +1,18 @@
+import sys
+import os
+from pyspark.sql.functions import *
+os.environ['JAVA_HOME'] = "C:\Java\jre-1.8"
+os.environ['SPARK_HOME'] = "C:\Spark\spark-3.4.1-bin-hadoop3"
+os.environ['HADOOP_HOME'] = "C:\hadoop\hadoop-3.3.5"
+os.environ['PYSPARK_PYTHON'] = "python"
+sys.path.append("C:\Spark\spark-3.4.1-bin-hadoop3\python")
+sys.path.append("C:\Spark\spark-3.4.1-bin-hadoop3\python\lib")
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master("local").appName("Word Count").getOrCreate()
+print(spark)
+
+data = [("Banana",1,"USA"),("Banana",2,"USA"), ("Carrots",4,"USA"), ("Beans",5,"USA")]
+columns = ["Product","Amount","Country"]
+df=spark.createDataFrame(data,columns)
+df.printSchema()
+df.show(truncate=False)
